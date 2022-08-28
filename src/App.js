01d047583ee05b5b581b5c2fa642ee072c242310
Pele12324 <48065727+Pele12324@users.noreply.github.com>
2022-08-28 19:12:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div>Free V bucks get ip grabbed bitch<br>suck my cock!</br></div>;
+  return <div>Free V bucks get ip grabbed bitch<br/>suck my cock!</div>;
 }
 
 export default App;
